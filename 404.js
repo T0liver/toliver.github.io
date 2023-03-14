d9bccd80f9bb0604a941c7b204f2b89e81c72b72
ut1s <110339660+ut1s@users.noreply.github.com>
2023-03-14 15:26:52 +0100

    Update 404 search #2

--- 404.js
+++ 404.js
@@ -1,25 +1,29 @@
-//let srchval = document.getElementById("search").value;
-
-//import { sites } from "./sites";
-var sites = Array("https://www.harmonikum.co/?s=", "https://www.vatera.hu/listings/index.php?q=", "https://www.webbeteg.hu/kereses_talalatok?p=", "https://www.jofogas.hu/magyarorszag?q=", "https://www.gyakorikerdesek.hu/kereses.php?keres=", "https://www.answers.com/search?q=", "https://www.reddit.com/search/?q=", "https://www.php.net/manual-lookup.php?pattern=", "https://www.pornhub.com/video/search?search=", "https://twitter.com/search?q=", "https://web.archive.org/web/*/", "https://www.imdb.com/find/?q=", "https://stackoverflow.com/search?q=", "https://hu.wikipedia.org/w/index.php?go=Menj&search=", "https://github.com/search?q=", "https://www.pexels.com/hu-hu/kereses/");
-
-function search() {
-  if (document.getElementById("search").value == "") {
-    document.getElementById("search").placeholder = "Kérem, írjon be valamit!";
-  } else {
-    //document.body.style.backgroundColor = "#ffffff";
+function searchterm() {
+  var srch = document.getElementById("search").value;
+  if (srch) {
     var randomsite = sites[Math.floor(Math.random() * sites.length)];
-    window.location.href = randomsite + document.getElementById("search").value;
+    var url = randomsite + srch;
+    window.location.href = url;
+    return false;
+  } else {
+    return false;
   }
-  /*const queryString = window.location.search;
-    const queryValue = queryString.split('=')[1];
-    document.getElementById("search").placeholder = queryValue;
-    if (queryValue == "") {
-      document.getElementById("search").placeholder = "KÉREM ÍRJON BE VALAMIT!";
-    } else {
-      //document.body.style.backgroundColor = "#ffffff";
-      var randomsite = sites[Math.floor(Math.random() * sites.length)];
-      window.location.href = randomsite + document.getElementById("search").value;
-    }*/
-  
 }
+
+var sites = Array(
+  "https://www.harmonikum.co/?s=",
+"https://www.vatera.hu/listings/index.php?q=",
+"https://www.webbeteg.hu/kereses_talalatok?p=",
+"https://www.jofogas.hu/magyarorszag?q=",
+"https://www.gyakorikerdesek.hu/kereses.php?keres=",
+"https://www.answers.com/search?q=",
+"https://www.reddit.com/search/?q=",
+"https://www.php.net/manual-lookup.php?pattern=",
+"https://www.pornhub.com/video/search?search=",
+"https://twitter.com/search?q=",
+"https://web.archive.org/web/*/",
+"https://www.imdb.com/find/?q=",
+"https://stackoverflow.com/search?q=",
+"https://hu.wikipedia.org/w/index.php?go=Menj&search=",
+"https://github.com/search?q=",
+"https://www.pexels.com/hu-hu/kereses/");
